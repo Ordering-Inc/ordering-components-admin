0c8d02830bcab6b08999a08729f0bde5a3e364f7
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-06-05 03:30:59 +0800

    add filters

--- src/components/DriversList/index.js
+++ src/components/DriversList/index.js
@@ -24,6 +24,26 @@ export const DriversList = (props) => {
    */
   const [driversList, setDriversList] = useState({ drivers: [], loading: true, error: null })
   /**
+   * Array to save online drivers
+   */
+  const [onlineDrivers, setOnlineDrivers] = useState([])
+  /**
+   * Array to save offline drivers
+   */
+  const [offlineDrivers, setOfflineDrivers] = useState([])
+  /**
+   * state for drivers online / offline filter
+   */
+  const [driversIsOnline, setDriversIsOnline] = useState(true)
+  /**
+   * state for drivers busy / not busy sub filter
+   */
+  const [driversSubfilter, setDriversSubfilter] = useState({
+    busy: true,
+    notBusy: true
+  })
+
+  /**
    * Method to assign driver to order from API
    * @param {object} assign assigned order id and driver id
    */
@@ -42,6 +62,46 @@ export const DriversList = (props) => {
     } catch (err) {
       setDriverActionStatus({ ...driverActionStatus, loading: false, error: [err.message] })
     }
+  }
+
+  /**
+   * change online state for drivers
+   * @param {Boolean} isOnline 
+   */
+  const handleChangeDriverIsOnline = (isOnline) => {
+    setDriversIsOnline(isOnline)
+  }
+
+  /**
+   * sub filter for drivers
+   * @param {Object} subFilter 
+   */
+  const handleChangeDriversSubFilter = (subFilter) => {
+    setDriversSubfilter(subFilter)
+  }
+
+  /**
+   * Method to get online/offline drivers
+   * @param {Array} drivers
+   */
+  const getOnlineOfflineDrivers = (drivers) => {
+    let _onlineDrivers
+    let _offlineDrivers
+    if (driversSubfilter.busy && driversSubfilter.notBusy) {
+      _onlineDrivers = drivers.filter(driver => driver.enabled && driver.available)
+      _offlineDrivers = drivers.filter(driver => driver.enabled && !driver.available)
+    } else if (driversSubfilter.busy && !driversSubfilter.notBusy) {
+      _onlineDrivers = drivers.filter(driver => driver.enabled && driver.available && driver.busy)
+      _offlineDrivers = drivers.filter(driver => driver.enabled && !driver.available && driver.busy)
+    } else if (!driversSubfilter.busy && driversSubfilter.notBusy) {
+      _onlineDrivers = drivers.filter(driver => driver.enabled && driver.available && !driver.busy)
+      _offlineDrivers = drivers.filter(driver => driver.enabled && !driver.available && !driver.busy)
+    } else {
+      _onlineDrivers = []
+      _offlineDrivers = []
+    }
+    setOnlineDrivers(_onlineDrivers)
+    setOfflineDrivers(_offlineDrivers)
   }
 
   /**
@@ -64,6 +124,7 @@ export const DriversList = (props) => {
         loading: false,
         drivers: result
       })
+      getOnlineOfflineDrivers(result)
     } catch (err) {
       setDriversList({
         ...driversList,
@@ -73,9 +134,18 @@ export const DriversList = (props) => {
     }
   }
 
+  /**
+   * listen for busy/not busy filter
+   */
+
+  useEffect(() => {
+    getOnlineOfflineDrivers(driversList.drivers)
+  }, [driversSubfilter])
+
   useEffect(() => {
     if (drivers) {
       setDriversList({ ...driversList, drivers: drivers, loading: false })
+      getOnlineOfflineDrivers(drivers)
     } else {
       getDrivers()
     }
@@ -93,7 +163,13 @@ export const DriversList = (props) => {
         <UIComponent
           {...props}
           driversList={driversList}
+          onlineDrivers={onlineDrivers}
+          offlineDrivers={offlineDrivers}
           driverActionStatus={driverActionStatus}
+          driversIsOnline={driversIsOnline}
+          driversSubfilter={driversSubfilter}
+          handleChangeDriverIsOnline={handleChangeDriverIsOnline}
+          handleChangeDriversSubFilter={handleChangeDriversSubFilter}
           handleAssignDriver={handleAssignDriver}
         />
       )}
