1599c7f2adc997d26849a6a1130415ab17120449
Min <skyandarti@outlook.com>
2021-06-24 06:47:25 +0800

    Feat: BasicSettings

---
+++ src/components/BasicSettings/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * Component to manage Checkout page behavior without UI component
+ */
+export const BasicSettings = (props) => {
+  const {
+    UIComponent
+  } = props
+
+  return (
+    <>
+      {UIComponent && (
+        <UIComponent
+          {...props}
+        />
+      )}
+    </>
+  )
+}
+
+BasicSettings.propTypes = {
+  /**
+   * UI Component, this must be containt all graphic elements and use parent props
+   */
+  UIComponent: PropTypes.elementType,
+  // /**
+  //  * handler values from other components
+  //  */
+  // handlerValues: PropTypes.func,
+  /**
+   * Components types before Checkout
+   * Array of type components, the parent props will pass to these components
+   */
+  beforeComponents: PropTypes.arrayOf(PropTypes.elementType),
+  /**
+   * Components types after Checkout
+   * Array of type components, the parent props will pass to these components
+   */
+  afterComponents: PropTypes.arrayOf(PropTypes.elementType),
+  /**
+   * Elements before Checkout
+   * Array of HTML/Components elements, these components will not get the parent props
+   */
+  beforeElements: PropTypes.arrayOf(PropTypes.element),
+  /**
+   * Elements after Checkout
+   * Array of HTML/Components elements, these components will not get the parent props
+   */
+  afterElements: PropTypes.arrayOf(PropTypes.element)
+}
+
+BasicSettings.defaultProps = {
+  beforeComponents: [],
+  afterComponents: [],
+  beforeElements: [],
+  afterElements: []
+}
