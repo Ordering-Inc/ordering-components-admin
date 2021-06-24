eb9be08828e52b7ca67d361c79b8c3ffa8f850a5
Min <skyandarti@outlook.com>
2021-06-24 08:33:43 +0800

    Update: get config cateories

--- src/components/BasicSettings/index.js
+++ src/components/BasicSettings/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { useSession } from '../../contexts/SessionContext'
 
 /**
  * Component to manage Checkout page behavior without UI component
@@ -8,6 +9,54 @@ export const BasicSettings = (props) => {
   const {
     UIComponent
   } = props
+
+  const [{ token, loading }] = useSession()
+  const [actionStatus, setActionStatus] = useState({ loading: false, error: null, result: null })
+
+  useEffect(() => {
+    getCagegories();
+  }, [])
+    /**
+   * Method to get Configration List
+   */
+     const getCagegories = async () => {
+      if (loading) return
+      try {
+        setActionStatus({ ...actionStatus, loading: true })
+        const requestOptions = {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+          }
+        }
+  
+        const functionFetch = 'https://apiv4.ordering.co/api_version/language/project/config_categories'
+  
+        const response = await fetch(functionFetch, requestOptions)
+        const { error, result } = await response.json()
+        console.log(result);
+        if (!error) {
+          setActionStatus({
+            ...actionStatus,
+            loading: false,
+            result: result
+          })
+        } else {
+          setActionStatus({
+            ...actionStatus,
+            loading: true,
+            error: result
+          })
+        }
+      } catch (err) {
+        setActionStatus({
+          ...actionStatus,
+          loading: false,
+          error: err
+        })
+      }
+    }
 
   return (
     <>
