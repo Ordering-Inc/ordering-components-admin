dcf0c7f940455eb6be852f3ebe6b60b395882a9e
Min <skyandarti@outlook.com>
2021-06-24 20:16:41 +0800

    Update: params

--- _modules/components/BasicSettings/index.js
+++ _modules/components/BasicSettings/index.js
@@ -106,7 +106,7 @@ var BasicSettings = function BasicSettings(props) {
                   Authorization: "Bearer ".concat(token)
                 }
               };
-              functionFetch = "".concat(ordering.root, "/config_categories");
+              functionFetch = "".concat(ordering.root, "/config_categories?params=description");
               _context.next = 8;
               return fetch(functionFetch, requestOptions);
 
