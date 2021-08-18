4a57609da00b0768531fe074415681156cfcaeba
Min <skyandarti@outlook.com>
2021-08-19 07:32:49 +0800

    Update: businessbrandform

--- src/components/BusinessBrandForm/index.js
+++ src/components/BusinessBrandForm/index.js
@@ -6,7 +6,11 @@ import { useSession } from '../../contexts/SessionContext'
 export const BusinessBrandForm = (props) => {
   const {
     UIComponent,
-    handleSuccessAddBusinessBrand
+    handleSuccessAddBusinessBrand,
+    editMode,
+    brand,
+    handleUpdateBrandList,
+    brandList
   } = props
 
   const [ordering] = useApi()
@@ -18,6 +22,17 @@ export const BusinessBrandForm = (props) => {
    * Default fuction for business profile workflow
    */
   const handleUpdateClick = async () => {
+    if (editMode) {
+      updateBrand()
+    } else {
+      createBrand()
+    }
+  }
+
+  /**
+   * Method to create brand
+   */
+  const createBrand = async () => {
     try {
       setFormState({ ...formState, loading: true })
       const requestOptions = {
@@ -39,6 +54,62 @@ export const BusinessBrandForm = (props) => {
         })
         if (handleSuccessAddBusinessBrand) {
           handleSuccessAddBusinessBrand(content.result)
+        }
+      } else {
+        setFormState({
+          ...formState,
+          changes: formState.changes,
+          result: content,
+          loading: false
+        })
+      }
+    } catch (err) {
+      setFormState({
+        ...formState,
+        result: {
+          error: true,
+          result: err.message
+        },
+        loading: false
+      })
+    }
+  }
+
+  /**
+   * Method to update a brand
+   */
+  const updateBrand = async () => {
+    try {
+      setFormState({ ...formState, loading: true })
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(formState.changes)
+      }
+      const response = await fetch(`${ordering.root}/franchises/${brand?.id}`, requestOptions)
+      const content = await response.json()
+      if (!content?.error) {
+        setFormState({
+          ...formState,
+          changes: {},
+          result: content,
+          loading: false
+        })
+        if (handleUpdateBrandList) {
+          const _brands = brandList?.brands.map(item => {
+            if (item.id === content.result.id) {
+              return {
+                ...item,
+                name: content.result.name,
+                logo: content.result.logo
+              }
+            }
+            return item
+          })
+          handleUpdateBrandList(_brands)
         }
       } else {
         setFormState({
@@ -114,6 +185,18 @@ BusinessBrandForm.propTypes = {
    * UI Component, this must be containt all graphic elements and use parent props
    */
   UIComponent: PropTypes.elementType,
+  /**
+   * Object for a brand
+   */
+  brand: PropTypes.object,
+  /**
+   * Object for brand list
+   */
+  brandList: PropTypes.object,
+  /**
+  * Function to set a business state
+  */
+  handleUpdateBrandList: PropTypes.func,
   /**
    * Components types before business type filter
    * Array of type components, the parent props will pass to these components
