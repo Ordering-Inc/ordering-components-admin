91f0dfd11006a0e7386f905d68208cf461cd0b5b
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-11-12 19:14:51 +0800

    update default order types

--- _modules/components/BusinessSchedule/index.js
+++ _modules/components/BusinessSchedule/index.js
@@ -29,8 +29,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
-
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
@@ -394,75 +392,24 @@ var BusinessSchedule = function BusinessSchedule(props) {
    */
 
 
-  var handleSelectCopyTimes = function handleSelectCopyTimes(index, daysOfWeekIndex) {
-    var _selectedCopyDays = _toConsumableArray(selectedCopyDays);
+  var handleSelectCopyTimes = function handleSelectCopyTimes(index) {
+    setSelectedCopyDays([].concat(_toConsumableArray(selectedCopyDays), [index]));
+  };
+  /**
+   * Method to apply copy times
+   * @param {Number} daysOfWeekIndex index of week days
+   */
 
-    var _schedule = _toConsumableArray(schedule);
 
-    if (!_selectedCopyDays.includes(index)) {
-      var conflict = false;
-
-      for (var i = 0; i < _schedule[index].lapses.length; i++) {
-        if (isCheckConflict(_schedule[daysOfWeekIndex].lapses, _schedule[index].lapses[i], null)) {
-          conflict = true;
-        }
+  var handleApplyScheduleCopyTimes = function handleApplyScheduleCopyTimes(daysOfWeekIndex) {
+    var _schedule = schedule.map(function (option, index) {
+      if (selectedCopyDays.includes(index)) {
+        return schedule[daysOfWeekIndex];
       }
 
-      if (conflict) {
-        setIsConflict(true);
-        return;
-      }
+      return option;
+    });
 
-      _selectedCopyDays.push(index);
-
-      var _iterator = _createForOfIteratorHelper(_schedule[index].lapses),
-          _step;
-
-      try {
-        for (_iterator.s(); !(_step = _iterator.n()).done;) {
-          var laps = _step.value;
-
-          _schedule[daysOfWeekIndex].lapses.push(laps);
-        }
-      } catch (err) {
-        _iterator.e(err);
-      } finally {
-        _iterator.f();
-      }
-
-      _schedule[daysOfWeekIndex].lapses.sort(function (a, b) {
-        return convertMinutes(a.open) - convertMinutes(b.open);
-      });
-    } else {
-      _selectedCopyDays = _selectedCopyDays.filter(function (el) {
-        return el !== index;
-      });
-
-      var newLapses = _schedule[daysOfWeekIndex].lapses.filter(function (laps) {
-        var _iterator2 = _createForOfIteratorHelper(_schedule[index].lapses),
-            _step2;
-
-        try {
-          for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
-            var deleteLaps = _step2.value;
-
-            if (convertMinutes(laps.open) === convertMinutes(deleteLaps.open) && convertMinutes(laps.close) === convertMinutes(deleteLaps.close)) {
-              return false;
-            }
-          }
-        } catch (err) {
-          _iterator2.e(err);
-        } finally {
-          _iterator2.f();
-        }
-
-        return true;
-      });
-
-      _schedule[daysOfWeekIndex].lapses = newLapses;
-    }
-
-    setSelectedCopyDays(_selectedCopyDays);
     setSchedule(_schedule);
     setFormState(_objectSpread(_objectSpread({}, formState), {}, {
       changes: {
@@ -585,7 +532,8 @@ var BusinessSchedule = function BusinessSchedule(props) {
     setAddScheduleTime: setAddScheduleTime,
     handleChangeAddScheduleTime: handleChangeAddScheduleTime,
     openAddScheduleIndex: openAddScheduleIndex,
-    setOpenAddScheduleInex: setOpenAddScheduleInex
+    setOpenAddScheduleInex: setOpenAddScheduleInex,
+    handleApplyScheduleCopyTimes: handleApplyScheduleCopyTimes
   })));
 };
 
