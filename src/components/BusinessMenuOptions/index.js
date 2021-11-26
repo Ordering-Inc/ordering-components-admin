e76b37c9773cb421e9b2c468e088f08a3f62ebd7
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-11-26 22:49:06 +0800

    update schedule  function

--- src/components/BusinessMenuOptions/index.js
+++ src/components/BusinessMenuOptions/index.js
@@ -21,19 +21,6 @@ export const BusinessMenuOptions = (props) => {
   const [formState, setFormState] = useState({ loading: false, changes: {}, error: null })
   const [orderTypeState, setOrderTypeSate] = useState({})
   const [selectedProductsIds, setSelectedProductsIds] = useState([])
-  const [selectedCopyDays, setSelectedCopyDays] = useState([])
-  const [schedule, setSchedule] = useState([])
-  const [openAddScheduleIndex, setOpenAddScheduleInex] = useState(null)
-  const [isConflict, setIsConflict] = useState(false)
-  const [addScheduleTime, setAddScheduleTime] = useState({
-    open: { hour: 0, minute: 0 },
-    close: { hour: 23, minute: 59 }
-  })
-
-  /**
-   * Clean selectedCopyDays
-   */
-  const cleanSelectedCopyDays = () => setSelectedCopyDays([])
 
   /**
    * Update business menu name and comment
@@ -63,219 +50,6 @@ export const BusinessMenuOptions = (props) => {
       }
     })
     setOrderTypeSate({ ...orderTypeState, [orderType]: !orderTypeState[orderType] })
-  }
-
-  /**
-   * Method to control the business schedule time enable state
-   * @param {Number} daysOfWeekIndex index of week days
-   */
-  const handleScheduleTimeActiveState = (daysOfWeekIndex) => {
-    const _schedule = [...schedule]
-    _schedule[daysOfWeekIndex].enabled = !_schedule[daysOfWeekIndex].enabled
-    setSchedule(_schedule)
-    setFormState({
-      ...formState,
-      changes: {
-        ...formState.changes,
-        schedule: _schedule
-      }
-    })
-  }
-
-  /**
-   * Method to check contain conflict
-   */
-  const isCheckConflict = (lapses, newSchedule, index) => {
-    const openNew = newSchedule.open.hour * 60 + newSchedule.open.minute
-    const closeNew = newSchedule.close.hour * 60 + newSchedule.close.minute
-    for (let i = 0; i < lapses.length; i++) {
-      if (i !== index) {
-        const openOld = lapses[i].open.hour * 60 + lapses[i].open.minute
-        const closeOld = lapses[i].close.hour * 60 + lapses[i].close.minute
-
-        if (openNew <= openOld && closeNew >= closeOld) return true
-        if (openNew < openOld && closeNew > openOld) return true
-        if (openNew > openOld && closeNew < closeOld) return true
-        if (openNew < closeOld && closeNew > closeOld) return true
-      }
-    }
-
-    return false
-  }
-
-  const convertMinutes = ({ hour, minute }) => {
-    return hour * 60 + minute
-  }
-
-  /**
-   * Update schedule time
-   * @param {Number} daysOfWeekIndex index of week days
-   * @param {Boolean} isOpen open time if true, else close time
-   * @param {Boolean} isHour hour if true, else minute
-   * @param {Number} index index of schedule time
-   * @param {String} value changed value
-   */
-  const handleChangeTime = (daysOfWeekIndex, isOpen, isHour, index, value) => {
-    const _schedule = [...schedule]
-    let conflict
-    const selectedLaps = _schedule[daysOfWeekIndex].lapses[index]
-
-    if (isOpen) {
-      if (isHour) {
-        conflict = isCheckConflict(
-          _schedule[daysOfWeekIndex].lapses,
-          {
-            open: { hour: parseInt(value), minute: selectedLaps.open.minute },
-            close: selectedLaps.close
-          },
-          index
-        )
-        if (conflict) {
-          setIsConflict(true)
-        } else {
-          _schedule[daysOfWeekIndex].lapses[index].open.hour = parseInt(value)
-        }
-      } else {
-        conflict = isCheckConflict(
-          _schedule[daysOfWeekIndex].lapses,
-          {
-            open: { hour: selectedLaps.open.hour, minute: parseInt(value) },
-            close: selectedLaps.close
-          },
-          index
-        )
-        if (conflict) {
-          setIsConflict(true)
-        } else {
-          _schedule[daysOfWeekIndex].lapses[index].open.minute = parseInt(value)
-        }
-      }
-    } else {
-      if (isHour) {
-        conflict = isCheckConflict(
-          _schedule[daysOfWeekIndex].lapses,
-          {
-            open: selectedLaps.open,
-            close: { hour: parseInt(value), minute: selectedLaps.close.minute }
-          },
-          index
-        )
-        if (conflict) {
-          setIsConflict(true)
-        } else {
-          _schedule[daysOfWeekIndex].lapses[index].close.hour = parseInt(value)
-        }
-      } else {
-        conflict = isCheckConflict(
-          _schedule[daysOfWeekIndex].lapses,
-          {
-            open: selectedLaps.open,
-            close: { hour: selectedLaps.close.hour, minute: parseInt(value) }
-          },
-          index
-        )
-        if (conflict) {
-          setIsConflict(true)
-        } else {
-          _schedule[daysOfWeekIndex].lapses[index].close.minute = parseInt(value)
-        }
-      }
-    }
-    if (!conflict) {
-      _schedule[daysOfWeekIndex].lapses.sort((a, b) => convertMinutes(a.open) - convertMinutes(b.open))
-      setSchedule(_schedule)
-      setFormState({
-        ...formState,
-        changes: {
-          schedule: JSON.stringify(_schedule)
-        }
-      })
-    }
-  }
-
-  /**
-   * Method to delete the schedule time
-   * @param {Number} daysOfWeekIndex index of week days
-   * @param {Number} index index of schedule time
-   */
-  const handleDeleteScheduleTime = (daysOfWeekIndex, index) => {
-    const _schedule = [...schedule]
-    _schedule[daysOfWeekIndex].lapses.splice(index, 1)
-    setSchedule(_schedule)
-    setFormState({
-      ...formState,
-      changes: {
-        schedule: JSON.stringify(_schedule)
-      }
-    })
-  }
-
-  /**
-   * Update schedule time
-   * @param {Boolean} isOpen open time if true, else close time
-   * @param {Boolean} isHour hour if true, else minute
-   * @param {String} value changed value
-   */
-  const handleChangeAddScheduleTime = (isOpen, isHour, value) => {
-    if (isOpen) {
-      if (isHour) {
-        setAddScheduleTime({
-          ...addScheduleTime,
-          open: {
-            ...addScheduleTime.open,
-            hour: parseInt(value)
-          }
-        })
-      } else {
-        setAddScheduleTime({
-          ...addScheduleTime,
-          open: {
-            ...addScheduleTime.open,
-            minute: parseInt(value)
-          }
-        })
-      }
-    } else {
-      if (isHour) {
-        setAddScheduleTime({
-          ...addScheduleTime,
-          close: {
-            ...addScheduleTime.close,
-            hour: parseInt(value)
-          }
-        })
-      } else {
-        setAddScheduleTime({
-          ...addScheduleTime,
-          close: {
-            ...addScheduleTime.close,
-            minute: parseInt(value)
-          }
-        })
-      }
-    }
-  }
-
-  /**
-   * Method to add the schedule time
-   * @param {Number} daysOfWeekIndex index of week days
-   */
-  const handleAddScheduleTime = (daysOfWeekIndex) => {
-    const _schedule = [...schedule]
-    const conflict = isCheckConflict(_schedule[daysOfWeekIndex].lapses, addScheduleTime, null)
-    if (conflict) {
-      setIsConflict(true)
-    } else {
-      _schedule[daysOfWeekIndex].lapses.push(addScheduleTime)
-      _schedule[daysOfWeekIndex].lapses.sort((a, b) => convertMinutes(a.open) - convertMinutes(b.open))
-      setSchedule(_schedule)
-      setFormState({
-        ...formState,
-        changes: {
-          schedule: JSON.stringify(_schedule)
-        }
-      })
-    }
   }
 
   /**
@@ -395,32 +169,12 @@ export const BusinessMenuOptions = (props) => {
     }
   }
 
-  /**
-   * Method to copy times
-   * @param {Number} index selected index
-   * @param {Number} daysOfWeekIndex index of week days
-   */
-  const handleSelectCopyTimes = (index) => {
-    setSelectedCopyDays([...selectedCopyDays, index])
-  }
-
-  /**
-   * Method to apply copy times
-   * @param {Number} daysOfWeekIndex index of week days
-   */
-  const handleApplyScheduleCopyTimes = (daysOfWeekIndex) => {
-    const _schedule = schedule.map((option, index) => {
-      if (selectedCopyDays.includes(index)) {
-        return schedule[daysOfWeekIndex]
-      }
-      return option
-    })
-    setSchedule(_schedule)
+  const handleChangeScheduleState = (scheduleChanges) => {
     setFormState({
       ...formState,
       changes: {
         ...formState.changes,
-        schedule: _schedule
+        schedule: scheduleChanges
       }
     })
   }
@@ -448,18 +202,8 @@ export const BusinessMenuOptions = (props) => {
     if (Object.keys(menu).length) {
       const _selectedProductsIds = menu.products.reduce((ids, product) => [...ids, product.id], [])
       setSelectedProductsIds(_selectedProductsIds)
-      setSchedule(menu?.schedule)
     } else {
       setSelectedProductsIds([])
-      setSchedule([
-        { enabled: true, lapses: [{ open: { hour: 0, minute: 0 }, close: { hour: 23, minute: 59 } }] },
-        { enabled: true, lapses: [{ open: { hour: 0, minute: 0 }, close: { hour: 23, minute: 59 } }] },
-        { enabled: true, lapses: [{ open: { hour: 0, minute: 0 }, close: { hour: 23, minute: 59 } }] },
-        { enabled: true, lapses: [{ open: { hour: 0, minute: 0 }, close: { hour: 23, minute: 59 } }] },
-        { enabled: true, lapses: [{ open: { hour: 0, minute: 0 }, close: { hour: 23, minute: 59 } }] },
-        { enabled: true, lapses: [{ open: { hour: 0, minute: 0 }, close: { hour: 23, minute: 59 } }] },
-        { enabled: true, lapses: [{ open: { hour: 0, minute: 0 }, close: { hour: 23, minute: 59 } }] }
-      ])
       setOrderTypeSate({
         delivery: false,
         pickup: false,
@@ -483,22 +227,7 @@ export const BusinessMenuOptions = (props) => {
             handleCheckOrderType={handleCheckOrderType}
             handleUpdateBusinessMenuOption={handleUpdateBusinessMenuOption}
             handleAddBusinessMenuOption={handleAddBusinessMenuOption}
-            scheduleTimes={schedule}
-            addScheduleTime={addScheduleTime}
-            setAddScheduleTime={setAddScheduleTime}
-            handleScheduleTimeActiveState={handleScheduleTimeActiveState}
-            handleChangeTime={handleChangeTime}
-            handleDeleteScheduleTime={handleDeleteScheduleTime}
-            handleChangeAddScheduleTime={handleChangeAddScheduleTime}
-            handleAddScheduleTime={handleAddScheduleTime}
-            openAddScheduleIndex={openAddScheduleIndex}
-            setOpenAddScheduleInex={setOpenAddScheduleInex}
-            isConflict={isConflict}
-            setIsConflict={setIsConflict}
-            selectedCopyDays={selectedCopyDays}
-            cleanSelectedCopyDays={cleanSelectedCopyDays}
-            handleSelectCopyTimes={handleSelectCopyTimes}
-            handleApplyScheduleCopyTimes={handleApplyScheduleCopyTimes}
+            handleChangeScheduleState={handleChangeScheduleState}
           />
         )
       }
