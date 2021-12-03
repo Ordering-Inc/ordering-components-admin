25155405d5c0fd8ead815acc20ee0710116de362
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-12-04 04:30:35 +0800

    build

--- _modules/components/DriversList/index.js
+++ _modules/components/DriversList/index.js
@@ -434,9 +434,34 @@ var DriversList = function DriversList(props) {
       }));
     };
 
+    var handleTrackingDriver = function handleTrackingDriver(trackingData) {
+      var drivers = [];
+      drivers = driversList.drivers.filter(function (_driver) {
+        if (_driver.id === trackingData.driver_id) {
+          if (typeof trackingData.location === 'string') {
+            var trackingLocation = trackingData.location;
+
+            var _location = trackingLocation.replaceAll('\\', '');
+
+            var location = JSON.parse(_location);
+            _driver.location = location;
+          } else {
+            _driver.location = trackingData.location;
+          }
+        }
+
+        return true;
+      });
+      setDriversList(_objectSpread(_objectSpread({}, driversList), {}, {
+        drivers: drivers
+      }));
+    };
+
     socket.on('drivers_update', handleUpdateDriver);
+    socket.on('tracking_driver', handleTrackingDriver);
     return function () {
       socket.off('drivers_update', handleUpdateDriver);
+      socket.off('tracking_driver', handleTrackingDriver);
     };
   }, [socket, session === null || session === void 0 ? void 0 : session.loading, driversList.drivers]);
   (0, _react.useEffect)(function () {
