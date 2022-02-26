a4c3dcdac4503a1525ee44d85a1d3c9f2e6ccc4d
Jesus David <jesusdpch00@gmail.com>
2022-02-26 15:55:35 -0400

    [FIX] lines

--- src/components/GoogleTagManager/index.js
+++ src/components/GoogleTagManager/index.js
@@ -1,5 +1,5 @@
-import propTypes from 'prop-types'
 import React, { useEffect } from 'react'
+import propTypes from 'prop-types'
 
 export const GoogleTagManager = ({ children, tagId }) => {
   useEffect(() => {
