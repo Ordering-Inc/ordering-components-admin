47413ca9ed984915fccd5ac5cd2b045d7d0b6535
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-03-02 02:28:36 +0800

    add inventoried item

--- src/components/BusinessSpreadSheet/index.js
+++ src/components/BusinessSpreadSheet/index.js
@@ -166,7 +166,7 @@ export const BusinessSpreadSheet = (props) => {
                     name: row.name,
                     description: row.description ? row.description : ' ',
                     price: row.price ? row.price : 0,
-                    offer_price: row?.offer_price || 0,
+                    inventoried: row?.inventoried || false,
                     category_id: id,
                     business_id: business?.id
                   }
