bd6421ec7259e8c7ababd1c5edcab2ab87ad86f7
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-05-02 18:49:31 +0800

    [Update] reset configs for logout

--- src/contexts/ConfigContext/index.js
+++ src/contexts/ConfigContext/index.js
@@ -91,7 +91,10 @@ export const ConfigProvider = ({ children }) => {
   }
 
   useEffect(() => {
-    if (ordering?.project === null) return
+    if (ordering?.project === null) {
+      setState({ loading: true, configs: {} })
+      return
+    }
     if (!languageState.loading) {
       refreshConfigs()
     }
