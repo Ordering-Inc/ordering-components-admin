80acbea6def379c86ac0dd0399ab807c84bcb5d6
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-05-07 00:16:15 +0800

    build

---
+++ _modules/components/PaymentOptionSquare/index.js
@@ -0,0 +1,346 @@
+"use strict";
+
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.PaymentOptionSquare = void 0;
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _ApiContext = require("../../contexts/ApiContext");
+
+var _SessionContext = require("../../contexts/SessionContext");
+
+var _ToastContext = require("../../contexts/ToastContext");
+
+var _LanguageContext = require("../../contexts/LanguageContext");
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var PaymentOptionSquare = function PaymentOptionSquare(props) {
+  var UIComponent = props.UIComponent,
+      business = props.business,
+      businessPaymethods = props.businessPaymethods,
+      handleSuccessPaymethodUpdate = props.handleSuccessPaymethodUpdate;
+
+  var _useToast = (0, _ToastContext.useToast)(),
+      _useToast2 = _slicedToArray(_useToast, 2),
+      showToast = _useToast2[1].showToast;
+
+  var _useLanguage = (0, _LanguageContext.useLanguage)(),
+      _useLanguage2 = _slicedToArray(_useLanguage, 2),
+      t = _useLanguage2[1];
+
+  var _useApi = (0, _ApiContext.useApi)(),
+      _useApi2 = _slicedToArray(_useApi, 1),
+      ordering = _useApi2[0];
+
+  var _useSession = (0, _SessionContext.useSession)(),
+      _useSession2 = _slicedToArray(_useSession, 1),
+      token = _useSession2[0].token;
+
+  var _useState = (0, _react.useState)({
+    url: null,
+    loading: false,
+    error: null
+  }),
+      _useState2 = _slicedToArray(_useState, 2),
+      squareUrlState = _useState2[0],
+      setSquareUrlState = _useState2[1];
+
+  var _useState3 = (0, _react.useState)(null),
+      _useState4 = _slicedToArray(_useState3, 2),
+      squareData = _useState4[0],
+      setSquareData = _useState4[1];
+
+  var _useState5 = (0, _react.useState)({
+    loading: false,
+    error: null
+  }),
+      _useState6 = _slicedToArray(_useState5, 2),
+      actionState = _useState6[0],
+      setActionState = _useState6[1];
+  /**
+   * Method to get connect url
+   */
+
+
+  var handleGetConnectUrl = /*#__PURE__*/function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee() {
+      var requestOptions, url, response, _yield$response$json, status, result;
+
+      return _regenerator.default.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.prev = 0;
+              setSquareUrlState(_objectSpread(_objectSpread({}, squareUrlState), {}, {
+                loading: true
+              }));
+              requestOptions = {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  business_id: business === null || business === void 0 ? void 0 : business.id,
+                  project_name: ordering === null || ordering === void 0 ? void 0 : ordering.project
+                })
+              };
+              url = 'https://plugins-live.ordering.co/square/oauth/oauth';
+              _context.next = 6;
+              return fetch(url, requestOptions);
+
+            case 6:
+              response = _context.sent;
+              _context.next = 9;
+              return response.json();
+
+            case 9:
+              _yield$response$json = _context.sent;
+              status = _yield$response$json.status;
+              result = _yield$response$json.result;
+
+              if (status === 'ok') {
+                setSquareUrlState(_objectSpread(_objectSpread({}, squareUrlState), {}, {
+                  loading: false,
+                  url: result === null || result === void 0 ? void 0 : result.data.url
+                }));
+              } else {
+                setSquareUrlState(_objectSpread(_objectSpread({}, squareUrlState), {}, {
+                  loading: false,
+                  error: result
+                }));
+              }
+
+              _context.next = 18;
+              break;
+
+            case 15:
+              _context.prev = 15;
+              _context.t0 = _context["catch"](0);
+              setSquareUrlState(_objectSpread(_objectSpread({}, squareUrlState), {}, {
+                loading: false,
+                error: [_context.t0.message]
+              }));
+
+            case 18:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee, null, [[0, 15]]);
+    }));
+
+    return function handleGetConnectUrl() {
+      return _ref.apply(this, arguments);
+    };
+  }();
+  /**
+   * Method to connect with Square
+   */
+
+
+  var handleConnectSquare = function handleConnectSquare() {
+    var connect = window.open(squareUrlState.url, '_blank', 'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,clearcache=yes');
+    var interval = setInterval(function () {
+      if (!connect.closed) {
+        connect.postMessage('data', ordering.url);
+      } else {
+        clearInterval(interval);
+      }
+    }, 200);
+    var timeout = null;
+    window.addEventListener('message', function (e) {
+      if (e.origin.indexOf('.ordering.co') === -1) {
+        return;
+      }
+
+      clearInterval(interval);
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        connect.postMessage('close', ordering.url);
+
+        if (e.data) {
+          var data = e.data.paymethod_credentials;
+          setSquareData({
+            sandbox: data.sandbox,
+            data: {
+              application_id: data.data.application_id,
+              location_id: data.data.location_id
+            },
+            data_sandbox: {
+              application_id: data.data_sandbox.application_id,
+              location_id: data.data_sandbox.location_id
+            }
+          });
+          connect.close();
+        }
+      }, 1000);
+    });
+  };
+  /**
+   * Method to update the business paymethod option
+   * @param {Number} paymethodId business paymethod id to delete
+   * @param {Object} options parameters to update
+   */
+
+
+  var handleSavePaymethod = /*#__PURE__*/function () {
+    var _ref2 = _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee2(paymethodId) {
+      var requestOptions, response, content, updatedPaymethods;
+      return _regenerator.default.wrap(function _callee2$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              _context2.prev = 0;
+              showToast(_ToastContext.ToastType.Info, t('LOADING', 'Loading'));
+              setActionState(_objectSpread(_objectSpread({}, actionState), {}, {
+                loading: true
+              }));
+              requestOptions = {
+                method: 'PUT',
+                headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: "Bearer ".concat(token),
+                  'x-app-x': ordering === null || ordering === void 0 ? void 0 : ordering.appId
+                },
+                body: JSON.stringify({
+                  sandbox: squareData === null || squareData === void 0 ? void 0 : squareData.sandbox,
+                  data: JSON.stringify(squareData === null || squareData === void 0 ? void 0 : squareData.data),
+                  data_sandbox: JSON.stringify(squareData === null || squareData === void 0 ? void 0 : squareData.data_sandbox)
+                })
+              };
+              _context2.next = 6;
+              return fetch("".concat(ordering.root, "/business/").concat(business.id, "/paymethods/").concat(paymethodId), requestOptions);
+
+            case 6:
+              response = _context2.sent;
+              _context2.next = 9;
+              return response.json();
+
+            case 9:
+              content = _context2.sent;
+
+              if (!content.error) {
+                updatedPaymethods = businessPaymethods.map(function (paymethod) {
+                  if (paymethod.id === paymethodId) {
+                    Object.assign(paymethod, content.result);
+                  }
+
+                  return paymethod;
+                });
+                handleSuccessPaymethodUpdate(updatedPaymethods);
+                showToast(_ToastContext.ToastType.Success, t('PAYMETHOD_SAVED', 'Payment method saved'));
+              }
+
+              setActionState({
+                loading: false,
+                error: content.error ? content.result : null
+              });
+              _context2.next = 17;
+              break;
+
+            case 14:
+              _context2.prev = 14;
+              _context2.t0 = _context2["catch"](0);
+              setActionState({
+                error: [_context2.t0.message],
+                loading: false
+              });
+
+            case 17:
+            case "end":
+              return _context2.stop();
+          }
+        }
+      }, _callee2, null, [[0, 14]]);
+    }));
+
+    return function handleSavePaymethod(_x) {
+      return _ref2.apply(this, arguments);
+    };
+  }();
+
+  (0, _react.useEffect)(function () {
+    handleGetConnectUrl();
+  }, []);
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, UIComponent && /*#__PURE__*/_react.default.createElement(UIComponent, _extends({}, props, {
+    squareUrlState: squareUrlState,
+    squareData: squareData,
+    handleConnectSquare: handleConnectSquare,
+    handleSavePaymethod: handleSavePaymethod
+  })));
+};
+
+exports.PaymentOptionSquare = PaymentOptionSquare;
+PaymentOptionSquare.propTypes = {
+  /**
+   * UI Component, this must be containt all graphic elements and use parent props
+   */
+  UIComponent: _propTypes.default.elementType,
+
+  /**
+    * Components types before business promotions
+    * Array of type components, the parent props will pass to these components
+    */
+  beforeComponents: _propTypes.default.arrayOf(_propTypes.default.elementType),
+
+  /**
+   * Components types after business promotions
+   * Array of type components, the parent props will pass to these components
+   */
+  afterComponents: _propTypes.default.arrayOf(_propTypes.default.elementType),
+
+  /**
+   * Elements before business promotions
+   * Array of HTML/Components elements, these components will not get the parent props
+   */
+  beforeElements: _propTypes.default.arrayOf(_propTypes.default.element),
+
+  /**
+   * Elements after business promotions
+   * Array of HTML/Components elements, these components will not get the parent props
+   */
+  afterElements: _propTypes.default.arrayOf(_propTypes.default.element)
+};
+PaymentOptionSquare.defaultProps = {
+  beforeComponents: [],
+  afterComponents: [],
+  beforeElements: [],
+  afterElements: []
+};
