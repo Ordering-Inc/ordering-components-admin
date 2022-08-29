3a7bb9dfc46122840f7fd5e4b3ec3ece0fadcc42
carlosg-ordering <carlos.g@ordering.co>
2022-08-29 09:28:37 -0500

    Merge pull request #309 Merge pull request #310 Merge pull request #311

--- src/index.js
+++ src/index.js
@@ -114,6 +114,7 @@ import { MenuControl } from './components/MenuControl'
 import { Messages } from './components/Messages'
 import { MetaFields } from './components/MetaFields'
 import { MomentOption } from './components/MomentOption'
+import { MultiCountrySettings } from './components/MultiCountrySettings'
 import { MyOrders } from './components/MyOrders'
 import { MyOrdersList } from './components/MyOrdersList'
 import { OpenCartListing } from './components/OpenCartListing'
@@ -341,6 +342,7 @@ export {
   MomentOption,
   MyOrders,
   MyOrdersList,
+  MultiCountrySettings,
   OpenCartListing,
   OpenCartsDetail,
   OpenCartFilter,
