8d883dc4f27731267ff4cf10c3871a70bc7da0e1
carlosg-ordering <carlos.g@ordering.co>
2023-02-08 12:05:25 -0600

    [FEAT] socket improvements

--- src/components/OrdersManage/index.js
+++ src/components/OrdersManage/index.js
@@ -577,7 +577,7 @@ export const OrdersManage = (props) => {
   }
 
   const handleUpdateOrder = () => {
-    getOrderNumbersByStatus()
+    // getOrderNumbersByStatus()
   }
   useEffect(() => {
     socket.on('update_order', handleUpdateOrder)
@@ -597,6 +597,18 @@ export const OrdersManage = (props) => {
     } else {
       socket.join(`orders_${user?.id}`)
       socket.join(`messages_orders_${user?.id}`)
+    }
+
+    return () => {
+      if (!user) return
+      socket.leave('drivers')
+      if (user.level === 0) {
+        socket.leave('orders')
+        socket.leave('messages_orders')
+      } else {
+        socket.leave(`orders_${user?.id}`)
+        socket.leave(`messages_orders_${user?.id}`)
+      }
     }
   }, [socket, loading, user])
 
