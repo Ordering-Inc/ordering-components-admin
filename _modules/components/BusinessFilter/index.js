4fa3f98a2bd068a1d09019398cad9ecb4edf7ffb
carlosg-ordering <carlos.g@ordering.co>
2023-05-08 09:12:22 -0600

    build

--- _modules/components/BusinessFilter/index.js
+++ _modules/components/BusinessFilter/index.js
@@ -1,50 +1,31 @@
 "use strict";
 
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.BusinessFilter = void 0;
-
 var _react = _interopRequireWildcard(require("react"));
-
 var _propTypes = _interopRequireDefault(require("prop-types"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
 function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
-
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
-
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 var BusinessFilter = function BusinessFilter(props) {
   var UIComponent = props.UIComponent;
   var initialFilterValues = {
@@ -64,29 +45,25 @@ var BusinessFilter = function BusinessFilter(props) {
   /**
    * This property is used to set in state the current value
   */
-
   var _useState = (0, _react.useState)(JSON.parse(JSON.stringify(initialFilterValues))),
-      _useState2 = _slicedToArray(_useState, 2),
-      filterValues = _useState2[0],
-      setFilterValues = _useState2[1];
+    _useState2 = _slicedToArray(_useState, 2),
+    filterValues = _useState2[0],
+    setFilterValues = _useState2[1];
+
   /**
    * Changer order Id
    * @param {object} changes filter values
    */
-
-
   var handleChangeValue = function handleChangeValue(changes) {
     setFilterValues(_objectSpread(_objectSpread({}, filterValues), changes));
   };
+
   /**
    * Change city
    * * @param {number} cityId city id of business
   */
-
-
   var handleChangeCity = function handleChangeCity(cityId) {
     var _cityIds = _toConsumableArray(filterValues.cityIds);
-
     if (!_cityIds.includes(cityId)) {
       _cityIds.push(cityId);
     } else {
@@ -94,20 +71,17 @@ var BusinessFilter = function BusinessFilter(props) {
         return _cityId !== cityId;
       });
     }
-
     setFilterValues(_objectSpread(_objectSpread({}, filterValues), {}, {
       cityIds: _cityIds
     }));
   };
+
   /**
    * Reset filter values
   */
-
-
   var handleResetFilterValues = function handleResetFilterValues() {
     setFilterValues(JSON.parse(JSON.stringify(initialFilterValues)));
   };
-
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, UIComponent && /*#__PURE__*/_react.default.createElement(UIComponent, _extends({}, props, {
     filterValues: filterValues,
     handleChangeValue: handleChangeValue,
@@ -115,31 +89,26 @@ var BusinessFilter = function BusinessFilter(props) {
     handleResetFilterValues: handleResetFilterValues
   })));
 };
-
 exports.BusinessFilter = BusinessFilter;
 BusinessFilter.propTypes = {
   /**
    * UI Component, this must be containt all graphic elements and use parent props
    */
   UIComponent: _propTypes.default.elementType,
-
   /**
    * Array that contains business types to filter
    */
   beforeComponents: _propTypes.default.arrayOf(_propTypes.default.elementType),
-
   /**
    * Components types after business type filter
    * Array of type components, the parent props will pass to these components
    */
   afterComponents: _propTypes.default.arrayOf(_propTypes.default.elementType),
-
   /**
    * Elements before business type filter
    * Array of HTML/Components elements, these components will not get the parent props
    */
   beforeElements: _propTypes.default.arrayOf(_propTypes.default.element),
-
   /**
    * Elements after business type filter
    * Array of HTML/Components elements, these components will not get the parent props
--- _modules/contexts/WebsocketContext/socket.js
+++ _modules/contexts/WebsocketContext/socket.js
@@ -4,38 +4,29 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.Socket = void 0;
-
 var _socket = _interopRequireDefault(require("socket.io-client"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 var Socket = /*#__PURE__*/function () {
   function Socket(_ref) {
     var url = _ref.url,
-        project = _ref.project,
-        accessToken = _ref.accessToken;
-
+      project = _ref.project,
+      accessToken = _ref.accessToken;
     _classCallCheck(this, Socket);
-
     this.url = url;
     this.project = project;
     this.accessToken = accessToken;
     this.queue = [];
   }
-
   _createClass(Socket, [{
     key: "connect",
     value: function connect() {
       var _this = this;
-
       this.socket = (0, _socket.default)(this.url, {
         extraHeaders: {
           Authorization: "Bearer ".concat(this.accessToken)
@@ -45,7 +36,6 @@ var Socket = /*#__PURE__*/function () {
       });
       this.socket.on('connect', function () {
         var item;
-
         while ((item = _this.queue.shift()) !== undefined) {
           if (item.action === 'on') {
             _this.on(item.event, item.func);
@@ -63,14 +53,12 @@ var Socket = /*#__PURE__*/function () {
     key: "getId",
     value: function getId() {
       var _this$socket;
-
       return (_this$socket = this.socket) === null || _this$socket === void 0 ? void 0 : _this$socket.id;
     }
   }, {
     key: "close",
     value: function close() {
       var _this$socket2;
-
       if ((_this$socket2 = this.socket) !== null && _this$socket2 !== void 0 && _this$socket2.connected) {
         this.socket.close();
       }
@@ -79,17 +67,14 @@ var Socket = /*#__PURE__*/function () {
     key: "join",
     value: function join(room) {
       var _this$socket3;
-
       if ((_this$socket3 = this.socket) !== null && _this$socket3 !== void 0 && _this$socket3.connected) {
         var paramRoom = null;
-
         if (_typeof(room) === 'object') {
           room.project = this.project;
           paramRoom = room;
         } else {
           paramRoom = "".concat(this.project, "_").concat(room);
         }
-
         this.socket.emit('join', paramRoom);
       } else {
         this.queue.push({
@@ -97,24 +82,20 @@ var Socket = /*#__PURE__*/function () {
           room: room
         });
       }
-
       return this;
     }
   }, {
     key: "leave",
     value: function leave(room) {
       var _this$socket4;
-
       if ((_this$socket4 = this.socket) !== null && _this$socket4 !== void 0 && _this$socket4.connected) {
         var paramRoom = null;
-
         if (_typeof(room) === 'object') {
           room.project = this.project;
           paramRoom = room;
         } else {
           paramRoom = "".concat(this.project, "_").concat(room);
         }
-
         this.socket.emit('leave', paramRoom);
       } else {
         this.queue.push({
@@ -122,16 +103,13 @@ var Socket = /*#__PURE__*/function () {
           room: room
         });
       }
-
       return this;
     }
   }, {
     key: "on",
     value: function on(event) {
       var _this$socket5;
-
       var func = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
-
       if ((_this$socket5 = this.socket) !== null && _this$socket5 !== void 0 && _this$socket5.connected) {
         this.socket.on(event, func);
       } else {
@@ -141,16 +119,13 @@ var Socket = /*#__PURE__*/function () {
           func: func
         });
       }
-
       return this;
     }
   }, {
     key: "off",
     value: function off(event) {
       var _this$socket6;
-
       var func = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
-
       if ((_this$socket6 = this.socket) !== null && _this$socket6 !== void 0 && _this$socket6.connected) {
         this.socket.off(event, func);
       } else {
@@ -160,12 +135,9 @@ var Socket = /*#__PURE__*/function () {
           func: func
         });
       }
-
       return this;
     }
   }]);
-
   return Socket;
 }();
-
 exports.Socket = Socket;
