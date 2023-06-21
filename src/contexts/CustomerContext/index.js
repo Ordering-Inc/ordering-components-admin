e4e774530f736fc8cc7c116821b4dd5d37ee3e10
Sky <min@ordering.co>
2023-06-21 07:15:24 -0600

    [Update]: site url feedback

--- src/contexts/CustomerContext/index.js
+++ src/contexts/CustomerContext/index.js
@@ -1,13 +1,8 @@
 import React, { useEffect, useState, useContext, createContext } from 'react'
-import { useSession } from '../SessionContext'
-import { useApi } from '../ApiContext'
 
 export const CustomerContext = createContext()
 
 export const CustomerProvider = ({ children, strategy }) => {
-  const [ordering] = useApi()
-  const [session] = useSession()
-
   const [state, setState] = useState({
     loading: false,
     user: null
@@ -51,34 +46,6 @@ export const CustomerProvider = ({ children, strategy }) => {
     setState({ ...state, loading: value })
   }
 
-    const [siteState, setSiteState] = useState({ site: null, loading: false, error: null })
-
-  /**
-   * Method to get the themes from API
-   */
-  const getSites = async () => {
-    try {
-      setSiteState({ ...siteState, loading: true })
-      const requestOptions = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${session?.token}`
-        }
-      }
-      const response = await fetch(`${ordering.root}/sites`, requestOptions)
-      const { error, result } = await response.json()
-      if (!error) {
-        const site = result.find(site => site.code === 'website')
-        setSiteState({ ...siteState, loading: false, site: site })
-      } else {
-        setSiteState({ ...siteState, loading: false, error: result })
-      }
-    } catch (err) {
-      setSiteState({ ...siteState, loading: false, error: [err.message] })
-    }
-  }
-
   const functions = {
     setLoadingCustomer,
     setUserCustomer,
@@ -87,7 +54,6 @@ export const CustomerProvider = ({ children, strategy }) => {
 
   useEffect(() => {
     getUserCustomerFromLocalStorage()
-    getSites()
   }, [])
 
   return (
