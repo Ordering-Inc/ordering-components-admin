9137a7d06aa91bf164147f183f6d081efcdc02ef
carlosg-ordering <carlos.g@ordering.co>
2023-07-06 10:23:28 -0600

    build

--- _modules/components/PaymentOptionWallet/index.js
+++ _modules/components/PaymentOptionWallet/index.js
@@ -181,7 +181,8 @@ var PaymentOptionWallet = function PaymentOptionWallet(props) {
                 'X-Socket-Id-X': socket === null || socket === void 0 ? void 0 : socket.getId()
               },
               body: JSON.stringify({
-                wallet_id: wallet.id
+                wallet_id: wallet.id,
+                user_id: user.id
               })
             });
           case 3:
@@ -235,7 +236,10 @@ var PaymentOptionWallet = function PaymentOptionWallet(props) {
                 Authorization: "Bearer ".concat(token),
                 'X-App-X': ordering.appId,
                 'X-Socket-Id-X': socket === null || socket === void 0 ? void 0 : socket.getId()
-              }
+              },
+              body: JSON.stringify({
+                user_id: user.id
+              })
             });
           case 3:
             response = _context3.sent;
