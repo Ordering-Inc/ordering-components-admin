42d57843c51d26e4c64c4351a28a3248115e1c99
carlosg-ordering <carlos.g@ordering.co>
2023-08-14 14:34:56 -0600

    Merge pull request #565

--- src/contexts/WebsocketContext/index.js
+++ src/contexts/WebsocketContext/index.js
@@ -39,26 +39,29 @@ export const WebsocketProvider = ({ settings, children }) => {
     return () => {
       socket && socket.close()
     }
-  }, [socket])
+  }, [socket, session?.user?.id])
 
   useEffect(() => {
-    if (socket?.socket) {
-      socket.socket.on('connect', () => {
-        window.localStorage.setItem('websocket-connected-date', new Date())
-        events.emit('websocket_connected')
-      })
+    if (!socket?.socket) return
+    let disconnectTimeout = null
+    let connectionErrorTimeout = null
 
-      socket.socket.on('disconnect', (reason) => {
-        if (reason === 'io server disconnect' && session.auth) {
-          window.setTimeout(socket.socket.connect(), 1000)
-        }
-      })
+    socket.socket.on('connect', () => {
+      window.localStorage.setItem('websocket-connected-date', new Date())
+      events.emit('websocket_connected')
+    })
 
-      socket.socket.on('connect_error', () => {
-        if (session.auth) {
-          window.setTimeout(socket.socket.connect(), 1000)
-        }
-      })
+    socket.socket.on('disconnect', () => {
+      disconnectTimeout = setTimeout(() => socket.socket.connect(), 1000)
+    })
+
+    socket.socket.on('connect_error', () => {
+      connectionErrorTimeout = setTimeout(() => socket.socket.connect(), 1000)
+    })
+
+    return () => {
+      clearInterval(disconnectTimeout)
+      clearInterval(connectionErrorTimeout)
     }
   }, [socket?.socket, session])
 
