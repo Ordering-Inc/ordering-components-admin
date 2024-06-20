cff90c5aa3358d8b267232297d4f97b9ee6c978f
Carlos Quiroz <carlos.q@ordering.co>
2024-06-20 15:35:05 -0400

    remove sentry

--- index-example.js
+++ index-example.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
 import App from './example/App'
 import { OrderingProvider } from './src/contexts/OrderingContext'
 
@@ -17,18 +15,6 @@ const configFile = {
     url: 'https://socket.ordering.co'
   }
 }
-
-Sentry.init({
-  environment: process.env.NODE_ENV,
-  dsn: 'https://c75f5b39a050458a920ce16a2878352a@o460529.ingest.sentry.io/5681465',
-  release: 'ordering-components-admin@' + process.env.npm_package_version,
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 0.2,
-  // Release health
-  autoSessionTracking: true,
-  integrations: [new Integrations.BrowserTracing()]
-})
 
 const wrapper = document.getElementById('app')
 ReactDOM.render(
